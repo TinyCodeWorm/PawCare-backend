6f99d290db8c85663521eb75fcad0d6a639cb0a4
Liu Ying <11775701@qq.com>
2021-08-04 20:13:51 -0700

    1.set up the framework

---
+++ go.mod
@@ -0,0 +1,14 @@
+module PetCare-backend/main
+
+go 1.16
+
+require (
+	github.com/auth0/go-jwt-middleware v1.0.1
+	github.com/form3tech-oss/jwt-go v3.2.3+incompatible
+	github.com/golang/mock v1.2.0 // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/olivere/elastic v6.2.23+incompatible // indirect
+	github.com/olivere/elastic/v7 v7.0.27 // indirect
+	google.golang.org/api v0.3.1 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+)
---
+++ index/index.go
@@ -0,0 +1,63 @@
+package index
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/olivere/elastic/v7"
+)
+
+func main() {
+	client, err := elastic.NewClient(
+		elastic.SetURL(ES_URL),
+		elastic.SetBasicAuth(ES_USERNAME, ES_PASSWORD))
+	if err != nil {
+		panic(err)
+	}
+
+	exists, err := client.IndexExists(POST_INDEX).Do(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	if !exists {
+		mapping := `{
+            "mappings": {
+                "properties": {
+                    "id":       { "type": "keyword" },
+                    "user":     { "type": "keyword" },
+                    "message":  { "type": "text" },
+                    "url":      { "type": "keyword", "index": false },
+                    "type":     { "type": "keyword", "index": false }
+                }
+            }
+        }`
+		_, err := client.CreateIndex(POST_INDEX).Body(mapping).Do(context.Background())
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	exists, err = client.IndexExists(USER_INDEX).Do(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	if !exists {
+		mapping := `{
+                        "mappings": {
+                                "properties": {
+                                        "username": {"type": "keyword"},
+                                        "password": {"type": "keyword"},
+                                        "age":      {"type": "long", "index": false},
+                                        "gender":   {"type": "keyword", "index": false}
+                                }
+                        }
+                }`
+		_, err = client.CreateIndex(USER_INDEX).Body(mapping).Do(context.Background())
+		if err != nil {
+			panic(err)
+		}
+	}
+	fmt.Println("Indexes are created.")
+}
