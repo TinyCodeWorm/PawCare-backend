a3ac8d4a01cbca0792f5cde5f138e6b0ffb84267
Liu Ying <11775701@qq.com>
2021-08-09 00:33:25 -0700

    getAllReactions is the sub function of getreactionsHandler

--- reaction.go
+++ reaction.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"reflect"
+
+	"github.com/olivere/elastic/v7"
+)
+
+func getAllReactions(w http.ResponseWriter) []Reaction {
+	query := elastic.NewMatchQuery("name", "")
+	query.ZeroTermsQuery("all")
+	searchResult, err := readFromES(query, REACTION_INDEX)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		fmt.Printf("Cannot get reaction data from index %v\n", err)
+	}
+
+	var reaction Reaction
+	var reactions []Reaction
+
+	for _, item := range searchResult.Each(reflect.TypeOf(reaction)) {
+		p := item.(Reaction)
+		reactions = append(reactions, p)
+	}
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		fmt.Printf("Failed to read reactions from Elasticsearch %v.\n", err)
+		return nil
+	}
+
+	return reactions
+}
